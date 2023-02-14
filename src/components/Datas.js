654bc0aed5ce08833956f66f79c1002375f92e5f
Elaf <skelaf2000@gmail.com>
2023-02-14 20:44:20 +0530

    Task 3

---
+++ src/components/Datas.js
@@ -0,0 +1,39 @@
+import React from "react"
+export default function Datas({cards}){
+    const msg = {
+        color:"#007070",
+    }
+    return(     
+        <>
+             <div> 
+                {
+                    cards.map((card)=>(
+                        <div className="myCard">
+                            <div className="description">
+                                <ul>
+                                    <li style={msg}><h2>Student Enrolled successfully</h2></li>
+                                    <li><b>Name : </b>{card["name"]}</li>
+                                    <li><b>Email : </b>{card["email"]}</li>
+                                    <li><b>Gender : </b>{card["gender"]}</li>
+                                    <li><b>Website : </b><a href={card["website"]}>{card["website"]}</a></li>
+                                    <li>
+                                        <b>Skills: </b>
+                                        {card["skills"].map((skill,index)=>(<span>{skill} , </span>))}
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="image">
+                                <img src={card["imageLink"]} />
+                            </div>
+                        </div>
+                        
+                    ))
+
+                }
+             </div>
+        </>
+        
+    )
+
+
+}
